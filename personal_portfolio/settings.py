73912587266e127ca0d5cbcee7991876af881e5f
Malina <malina16fnl@gmail.com>
2022-11-13 13:38:14 +0000

    allowed host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
+
 from pathlib import Path, PurePath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7$^fxa3i+7$et0qv+)7=jkp8=khun_+fyjczy09jw4gidazl=p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mbenchea.pythonanywhere.com']
 
 
 # Application definition
